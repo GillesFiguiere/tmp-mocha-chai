ee649fee79a0dac4ed9c8b046a050214dc6dfef0
Gilles Figuière <me@gillesfiguiere.com>
2019-11-19 10:56:39 +0100

    package.json with all latest dependencies and scripts to transpile and test minimal spec file with expect from chai import

---
+++ specs/kata.spec.ts
@@ -0,0 +1,10 @@
+import {expect} from 'chai';
+
+describe("Kata", () => {
+    it("Should be true", () => {
+        expect(true).to.be.true
+    })
+    it("Should be false", () => {
+        expect(false).to.be.false
+    })
+})
